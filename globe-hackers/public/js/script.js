43e6ee4cd317e27f914be21e033fc445d7a7d59a
“Alexandre <coma.alexandre@gmail.com>
2022-07-15 15:17:23 +0200

    initializing

---
+++ globe-hackers/public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("globe-hackers JS imported successfully!");
+  },
+  false
+);
---
+++ globe-hackers/utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/globe-hackers";
+
+module.exports = MONGO_URI;

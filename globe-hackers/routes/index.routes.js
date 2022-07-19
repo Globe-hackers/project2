64276f2a558dae846d5b6cbc0f7367c379751276
Stephanie-Tovar <stephanie.anne.tovar@gmail.com>
2022-07-19 16:30:58 +0200

    add functionality to display latest entries in DB in homepage

--- globe-hackers/routes/index.routes.js
+++ globe-hackers/routes/index.routes.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
+const Tip = require("../models/Tip.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
-});
+  Tip.find().sort({ createdAt: -1 }).limit(1)
+    .then((found) => {
+      res.render("index", { found })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 module.exports = router;

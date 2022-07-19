fc31a12c8fab55b88125b006484e5ca7fdf7299f
Stephanie-Tovar <stephanie.anne.tovar@gmail.com>
2022-07-19 16:36:42 +0200

    display recent stories in homepage

--- globe-hackers/routes/index.routes.js
+++ globe-hackers/routes/index.routes.js
@@ -3,7 +3,7 @@ const Tip = require("../models/Tip.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  Tip.find().sort({ createdAt: -1 }).limit(1)
+  Tip.find().sort({ createdAt: -1 }).limit(4)
     .then((found) => {
       res.render("index", { found })
     })
